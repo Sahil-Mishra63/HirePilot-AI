a3cb2c3ae0bb97feeacd7c47bf2c62fc17a2ae39
Subhadarshini-cod <subhadarshinirauta@gmail.com>
2026-09-01 01:53:49 +0530

    resume-upload system added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ResumeUpload } from './resume-upload/resume-upload';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'resume-upload',
+    component: ResumeUpload
+  }
+];
