768ec42dd72affcd857d648ec7e203a3d98f6626
TanushreeNayak-cse <23cse147.tanushreenayak@giet.edu>
2026-09-02 11:42:22 +0530

    Add AI assessment result report

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './page/landing/landing.component';
+import { AiAssessmentComponent } from './ai-assessment/ai-assessment.component';
+import { ResultReportComponent } from './resultReport/resultReport.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
+    { path: 'assess', component: AiAssessmentComponent },
+    {
+    path: 'result-report',
+    component: ResultReportComponent
+    }
+
 ];
